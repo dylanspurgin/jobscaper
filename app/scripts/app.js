f446cb1481d6fb5df3119a2c30165bc77be686fe
Dylan Spurgin <dspurgin@invodo.com>
2015-07-29 22:47:30 -0500

    Start to a basic ui with menu, list of jobs, and job detail page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,70 +4,62 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('jobscaper', ['ionic', 'jobscaper.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+      .state('app', {
+        url: '',
+        abstract: true,
+        templateUrl: 'templates/main.html',
+        controller: 'AppController'
+      })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+      //.state('app.search', {
+      //  url: '/search',
+      //  views: {
+      //    'menuContent': {
+      //      templateUrl: 'templates/search.html'
+      //    }
+      //  }
+      //})
+
+
+      .state('app.jobs', {
+        url: '/jobs',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/job-list.html',
+            controller: 'JobListController'
+          }
+        }
+      })
+      .state('app.job', {
+        url: '/job/:jobId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/job.html',
+            controller: 'JobController'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/jobs');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,56 +1,120 @@
-angular.module('starter.controllers', [])
+angular.module('jobscaper.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppController', function ($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
+
+  .controller('JobListController', function ($scope) {
+    $scope.jobs = [
+      {
+        id: 1,
+        name: '1910 Harrold St'
+      },
+      {
+        id: 2,
+        name: '2603 McGregor'
+      },
+      {
+        id: 3,
+        name: '11812 Tedford'
+      },
+      {
+        id: 4,
+        name: '666 UrMoms Hs'
+      }
+    ];
+  })
+
+  .controller('JobController', function ($scope, $stateParams) {
+
+    $scope.job = {
+      id: 1,
+      name: '1910 Harrold St',
+      clientContact: {
+        name: 'Billy Joe Philmore',
+        phone: '512 555 1212',
+        address1: '1920 Harrold St',
+        city: 'Austin',
+        state: 'Tx',
+        zip: '78704'
+      },
+      overview: 'Morbi in euismod urna, vitae congue odio. Nullam in interdum enim. Vestibulum tristique placerat felis, eget facilisis justo accumsan non. Curabitur a libero interdum, vestibulum ipsum eu, laoreet quam. Nunc in dapibus velit. Maecenas ac neque leo.',
+      tasks: [
+        {
+          id: 1,
+          complete: true,
+          name: 'Concrete Pathway',
+          materials: [
+            {
+              name: 'bag of cement',
+              quantity: 14
+            },
+            {
+              name: 'Gallon of stain',
+              quantity: 2
+            }
+          ]
+        },
+        {
+          id: 2,
+          complete: false,
+          name: 'Cedar Fence',
+          materials: [
+            {
+              name: '8\' metal post',
+              quantity: 18
+            },
+            {
+              name: '8x4 cedar picket',
+              quantity: 266
+            },
+            {
+              name: 'bag of cement',
+              quantity: 22
+            },
+            {
+              name: 'Gallon of stain',
+              quantity: 4
+            }
+          ]
+        }
+      ]
+    }
+
   });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
